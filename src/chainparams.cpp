03922e5931ebe701f0777427097e911a269c98b3
hkbewater <58812655+hkbewater@users.noreply.github.com>
2019-12-12 20:51:42 +0800

    undated magic bytes  (pchMessageStart0-3)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,10 +162,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x90;
-        pchMessageStart[1] = 0xc4;
-        pchMessageStart[2] = 0xfd;
-        pchMessageStart[3] = 0xe9;
+        pchMessageStart[0] = 0xba;
+        pchMessageStart[1] = 0xcc;
+        pchMessageStart[2] = 0xf7;
+        pchMessageStart[3] = 0xd9;
         vAlertPubKey = ParseHex("0000098d3ba6ba6e7423fa5cbd6a89e0a9a5348f88d332b44a5cb1a8b7ed2c1eaa335fc8dc4f012cb8241cc0bdafd6ca70c5f5448916e4e6f511bcd746ed57dc50");
         nDefaultPort = 51472;
         bnProofOfWorkLimit = ~uint256(0) >> 20; // PIVX starting difficulty is 1 / 2^12
